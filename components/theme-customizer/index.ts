12ff1811e0a4048171c8254721284d43d14e5f0a
Gyeong-Min Lee <tyler7688@gmail.com>
2025-12-09 23:05:53 +0900

    forgot to comment out the unused sidebar setting component

--- components/theme-customizer/index.ts
+++ components/theme-customizer/index.ts
@@ -3,6 +3,5 @@ export * from "./content-layout-selector";
 export * from "./preset-selector";
 export * from "./radius-selector";
 export * from "./reset-theme";
-export * from "./sidebar-mode-selector";
 export * from "./scale-selector";
 export * from "./panel";
